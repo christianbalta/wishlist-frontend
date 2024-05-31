7140d6d59162483a8eb987e85e01fc9daf4a7c25
Christian Balta <christian.balta@spsglobal.com>
2024-05-31 12:42:58 +0200

    Initial commit

---
+++ src/app/wishlist/wishlist.component.ts
@@ -0,0 +1,55 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
+import {Button, ButtonDirective} from 'primeng/button';
+import {CardModule} from "primeng/card";
+
+export interface WishlistItem {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  isReserved: boolean;
+}
+
+@Component({
+  selector: 'app-wishlist',
+  standalone: true,
+  imports: [
+    NgForOf,
+    Button,
+    CardModule,
+    ButtonDirective,
+    NgIf,
+  ],
+  templateUrl: './wishlist.component.html',
+  styleUrl: './wishlist.component.css'
+})
+export class WishlistComponent implements OnInit {
+  wishlistItems: WishlistItem[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.fetchWishlistItems();
+  }
+
+  fetchWishlistItems() {
+    this.http.get<WishlistItem[]>('http://localhost:5054/api/Wishlist')
+      .subscribe(items => this.wishlistItems = items);
+  }
+
+  reserveItem(item: any) {
+    this.http.put(`http://localhost:5054/api/Wishlist/${item.id}`, {
+      isReserved: true,
+      name: item.name,
+      description: item.description,
+      price: item.price,
+      id: item.id
+    })
+      .subscribe(() => {
+        this.fetchWishlistItems();
+      });
+  }
+}
